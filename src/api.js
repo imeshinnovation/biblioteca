efae155688dad9b41f4d1076ad23d21037f877a1
imeshinnovation <imeshinnovation@gmail.com>
2024-04-12 23:31:34 -0500

    Update

--- src/api.js
+++ src/api.js
@@ -2,10 +2,18 @@ const express = require("express")
 const app1 = express()
 const morgan = require("morgan")
 const env = require("dotenv").config().parsed
+const cors = require("cors")
+
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    optionsSuccessStatus: 200
+}
 
 const BaseDatos = require('./libs/dbs')
 BaseDatos.connect()
 
+app1.use(cors(corsOptions))
 app1.use(morgan("dev"))
 app1.use(express.json())
 app1.use(express.urlencoded({ extended: true }))
